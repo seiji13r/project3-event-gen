7deee7f5b9cf174ba1afe20223f67edf80644d9d
Jorge Baez <jibm_82@yahoo.com.mx>
2019-02-14 23:33:51 -0600

    Removed unused urls

--- deleteAll.js
+++ deleteAll.js
@@ -1,26 +1,23 @@
-const uuidv4 = require('uuid/v4');
-const axios = require("axios")
+const uuidv4 = require("uuid/v4");
+const axios = require("axios");
 uuidv4();
 
 const production = false;
 
-let eventsURL = "http://localhost:3001/epc-events" 
-let productsURL = "http://localhost:3001/api/products"
-let readersURL = "http://localhost:3001/api/readers"
+let eventsURL = "http://localhost:3001/epc-events";
 
 if (production) {
-  eventsURL = "https://project3-assets-overlord.herokuapp.com/epc-events" 
-  productsURL = "https://project3-assets-overlord.herokuapp.com/api/products"
-  readersURL = "https://project3-assets-overlord.herokuapp.com/api/readers"
+  eventsURL = "https://project3-assets-overlord.herokuapp.com/epc-events";
 }
 
 const deleteAll = () => {
-  axios.delete(eventsURL, {})
+  axios
+    .delete(eventsURL, {})
     .then(response => console.log(response))
-    .catch(error => console.log(error))
-}
+    .catch(error => console.log(error));
+};
 
 // Delete Events Products and Readers
 deleteAll();
 
-console.log("All Objects Deleted")
+console.log("All Objects Deleted");
